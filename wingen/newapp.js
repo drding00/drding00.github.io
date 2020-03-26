905bbcffa6e2c52c59577859cec940c3928874ff
drding00 <drding00@users.noreply.github.com>
2020-03-26 19:35:45 -0400

    Added app code
    Added new app file with some psudo code. This is going to be the version that will utilize localstorage.

---
+++ wingen/newapp.js
@@ -0,0 +1,12 @@
+
+// When the add name button is clicked, we should check to see if there is a localstorage item.
+// If there isn't one, one should be made, and the first name added. 
+// Else we should take the newly updated array of names and update the LS item. 
+
+// When the winner button is clicked, we should retrieve the array from LS, and run the generator.
+// When the generator is done, the selected name will be displayed.
+
+// When the clear button is clicked, we should delete/clear everything in the LS. 
+
+
+
