0b9080386f28454fa2daa1e7f156cf1e648f9905
drding00 <danielle.m.lyons@gmail.com>
2022-05-12 14:34:40 -0400

    deleted files
    accidently added files outside a directory. oops!

--- script.js
+++
@@ -1,7 +0,0 @@
-const mobileNav = document.querySelector('#mobile-nav');
-const mobileButton = document.querySelector('.mobile-button');
-
-mobileButton.addEventListener('click', () => {
-  mobileButton.classList.toggle('toggle-button');
-  mobileNav.classList.toggle('active');
-});
